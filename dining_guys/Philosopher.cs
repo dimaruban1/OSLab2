538bec18ba4ec445303ba6080c3d3589e572e911
dimaruban1 <145126723+dimaruban1@users.noreply.github.com>
2024-03-31 19:49:20 +0300

    init

---
+++ dining_guys/Philosopher.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dining_guys
+{
+    class Philosopher
+    {
+        int r;
+        public Stopwatch timer { get; private set; }
+
+        public string Name { get; set; }
+        public Semaphore LeftFork { get; set; }
+        public Semaphore RightFork { get; set; }
+
+        Random rnd;
+        int min;
+        int max;
+        int numberOfDishes = 20;
+        public Philosopher(int min, int max, string name, Semaphore leftFork, Semaphore rightFork)
+        {
+            rnd = new Random();
+            this.min = min;
+            this.max = max;
+            r = rnd.Next(min, max);
+            Name = name;
+            timer = Stopwatch.StartNew();
+            timer.Start();
+            LeftFork = leftFork;
+            RightFork = rightFork;
+        }
+        void Eat()
+        {
+            Console.WriteLine($"{Name} is eating...");
+            Thread.Sleep(rnd.Next(1000));
+            timer.Restart();
+        }
+        void Think()
+        {
+            Console.WriteLine($"{Name} is thinking about those {numberOfDishes} plates of pasta left to eat...");
+            Thread.Sleep(rnd.Next(max));
+        }
+        void ReleaseForks()
+        {
+            LeftFork.Release();
+            RightFork.Release();
+        }
+        void GetLeftFork() { LeftFork.WaitOne(); }
+        bool TryGetRightFork() { return RightFork.WaitOne((int)timer.ElapsedMilliseconds + r); }
+
+        public void Dine()
+        {
+            while (numberOfDishes > 0)
+            {
+                Thread.Sleep(r);
+                GetLeftFork();
+                if (TryGetRightFork())
+                {
+                    Eat();
+                    numberOfDishes--;
+                    ReleaseForks();
+                    Think();
+                    timer.Restart();
+                }
+                else
+                {
+                    LeftFork.Release();
+                    r = rnd.Next(min, max);
+                }
+            }
+        } 
+    }
+}
---
+++ dining_guys/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using dining_guys;
+
+
+const int numPhilosophers = 999;
+Stopwatch t = new Stopwatch();
+t.Start();
+// Initialize semaphores representing forks
+Semaphore[] forks = new Semaphore[numPhilosophers];
+for (int i = 0; i < numPhilosophers; i++)
+{
+    forks[i] = new Semaphore(1, 1); // Initialize each fork with count 1
+}
+
+// Create philosophers and start their dining process
+Philosopher[] philosophers = new Philosopher[numPhilosophers];
+Thread[] philosopherThreads = new Thread[numPhilosophers];
+for (int i = 0; i < numPhilosophers; i++)
+{
+    Semaphore leftFork = forks[i];
+    Semaphore rightFork = forks[(i + 1) % numPhilosophers]; // Circular arrangement of forks
+    philosophers[i] = new Philosopher(0, 10, "philosopher " + i + " ", leftFork, rightFork);
+    Thread philosopherThread = new Thread(philosophers[i].Dine);
+    philosopherThreads[i] = philosopherThread;
+    philosopherThread.Start();
+}
+
+// Wait for all philosopher threads to finish (this won't happen in this infinite loop)
+foreach (Thread philosopherThread in philosopherThreads)
+{
+    philosopherThread.Join();
+}
+Console.WriteLine(t.Elapsed.ToString());
